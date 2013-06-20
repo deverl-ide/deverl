92b333453e7d703f8f2830558b3cd8f1b56c86c6
Tommo <me@tomrichmond.co.uk>
2013-06-20 17:38:55 +0100

    Changed ide_shell to gen_server

--- proto/src/ide_shell.erl
+++ proto/src/ide_shell.erl
@@ -1,19 +1,40 @@
 -module(ide_shell).
 -include_lib("wx/include/wx.hrl").
--compile(export_all).
 
+-export([start/0, start/1, start_link/0, start_link/1, 
+	 init/1, terminate/2,  code_change/3,
+	 handle_info/2, handle_call/3, handle_cast/2, handle_event/2]).
+
+-behaviour(wx_object).
+
+-define(SHELL_TEXT_BOX, 001).
+
+%% The record containing the State.
+-record(state, {win, textctrl, input}).
 
 start() ->
-	loop([], 1, make_window()).
+  start([]).
+  
+start(Args) ->
+  wx_object:start(?MODULE, Args, []).
+  
+start_link() ->
+  start_link([]).
+  
+start_link(Args) ->
+  wx_object:start_link(?MODULE, Args, []).
 	
-make_window() ->
+%% Initialise the server's state
+init(Args) ->
 	Server = wx:new(),
 	Frame  = wxFrame:new(Server, -1, "Erlang Shell", [{size, {800, 300}}]),
 	Panel  = wxPanel:new(Frame),
+  
+  Input = "> ",
 	
   % The style of the text box
-	ShellTextBox = wxTextCtrl:new(Panel, 001, [{style, ?wxTE_MULTILINE}, 
-											   {size, {800, 300}}]),
+	ShellTextBox = wxTextCtrl:new(Panel, ?SHELL_TEXT_BOX, [{style, ?wxTE_MULTILINE}, 
+											   {size, {800, 300}}, {value, Input}]),
 	wxWindow:setForegroundColour(ShellTextBox, ?wxWHITE),
 	wxWindow:setBackgroundColour(ShellTextBox, ?wxBLACK),
 	wxWindow:setFont(ShellTextBox, wxFont:new(12, ?wxFONTFAMILY_TELETYPE, ?wxNORMAL, ?wxNORMAL,[])),
@@ -25,19 +46,54 @@ make_window() ->
 	
 	wxFrame:show(Frame),
 	
-	%wxTextCtrl:connect(ShellTextBox, char),
+	wxTextCtrl:connect(ShellTextBox, char),
 	
-	ShellTextBox.
+	{Frame, #state{win=Frame, textctrl=ShellTextBox, input=Input}}. %% Maintained at server
 	
-loop(Function, ArgNum, ShellTextBox) ->
-	wxTextCtrl:writeText(ShellTextBox, integer_to_list(ArgNum) ++ "> ").
-	%receive
-	%	#wx{id = 001, event = #wxKey{type = char}} ->
-	%		io:format("BAM")
-	%end,
-	%loop([], 1, ShellTextBox).
+%%%%% Callbacks %%%%%
+%% These are all called from the server %%
+handle_info({'EXIT',_, wx_deleted}, State) ->
+    {noreply,State};
+handle_info({'EXIT',_, shutdown}, State) ->
+    {noreply,State};
+handle_info({'EXIT',_, normal}, State) ->
+    {noreply,State};
+handle_info(Msg, State) ->
+    io:format("Got Info ~p~n",[Msg]),
+    {noreply,State}.
 
-		
-	
-	
-	
+handle_call(Msg, _From, State) ->
+    io:format("Got Call ~p~n",[Msg]),
+    {reply,ok,State}.
+
+handle_cast(Msg, State) ->
+    io:format("Got cast ~p~n",[Msg]),
+    {noreply,State}.
+
+%% This is where events are handled %%
+handle_event(#wx{event=#wxClose{}}, State = #state{win=Frame, input=Input}) ->
+    ok = wxFrame:setStatusText(Frame, "Closing...",[]),
+    {stop, normal, State};
+%% This is executed where char events are handled
+%% First, deal with the the case where the user enters a full stop
+handle_event(#wx{event=#wxKey{type=char, keyCode=46}}, State = #state{win=Frame, textctrl = TextCtrl, input = Input}) ->    %% Full stop
+    NewInput = Input ++ ".",
+    wxTextCtrl:writeText(TextCtrl, "\nYou have entered:\n"),
+    wxTextCtrl:writeText(TextCtrl, NewInput),
+    {noreply, State#state{input="> "}};
+%% We can deal with any key as we have above (i.e. the enter key)
+%% Now just deal with any char
+handle_event(#wx{event=#wxKey{type=char, keyCode=KeyCode}}, State = #state{win=Frame, textctrl = TextCtrl, input = Input}) ->
+    %% This currently treats everything as an ascii character
+    Key = io_lib:format("~c", [KeyCode]),
+    %% Update the state
+    NewInput = Input ++ Key,
+    %% Prompt again
+    wxTextCtrl:setValue(TextCtrl, NewInput),
+    {noreply, State#state{input=NewInput}}.
+
+code_change(_, _, State) ->
+    {stop, not_yet_implemented, State}.
+
+terminate(_Reason, _State) ->
+    wx:destroy().
