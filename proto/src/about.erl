a0ba81683d18d7825e8329a1e4b098fe66cc6238
mdq3 <mdq3@sybil.(none)>
2013-07-03 11:18:40 +0100

    Committer: mdq3 <mdq3@sybil.(none)> 	modified:   proto/src/about.erl

--- proto/src/about.erl
+++ proto/src/about.erl
@@ -22,17 +22,18 @@ start(Config) ->
 
 init(Args) ->
 	Parent = proplists:get_value(parent, Args),
-	Frame = wxFrame:new(wx:null(), ?wxID_ANY, "About", [{size,{500, 500}}, 
-														{style, ?wxSTAY_ON_TOP bor
-																?wxSYSTEM_MENU bor
-																?wxFRAME_NO_TASKBAR bor
-																?wxCLOSE_BOX}]),
+	Frame = wxFrame:new(Parent, ?wxID_ANY, "About", [{size,{500, 500}}, 
+													 {style, ?wxSTAY_ON_TOP bor
+															 ?wxSYSTEM_MENU bor
+															 ?wxFRAME_NO_TASKBAR bor
+															 ?wxCLOSE_BOX}]),
 	Panel = wxPanel:new(Frame),
 	MainSizer   = wxBoxSizer:new(?wxVERTICAL),
+	wxPanel:setSizer(Panel, MainSizer),
 	
 	TabbedPane  = wxNotebook:new(Panel, ?TABBED_PANE, []),
 	InfoPane    = wxStaticText:new(TabbedPane, ?INFO_PANE, []),
-	  %set_info(InfoPane),
+	  %set_info(InfoPane, ?INFO),
 	LicensePane = wxStaticText:new(TabbedPane, ?LICENSE_PANE, []),
 	CloseButton = wxButton:new(Panel, ?wxID_EXIT, [{label, "&Close"}]),
 	
@@ -42,23 +43,20 @@ init(Args) ->
 	wxSizer:add(MainSizer, TabbedPane, [{flag, ?wxEXPAND}, {proportion, 1}]),
 	wxSizer:add(MainSizer, CloseButton, [{flag, ?wxALIGN_RIGHT}]),
 	
-	wxPanel:setSizer(Panel, MainSizer),
 	wxFrame:centerOnParent(Frame),
 	wxFrame:show(Frame),
 	
-	wxButton:connect(CloseButton, command_button_clicked),
+	wxFrame:connect(CloseButton, command_button_clicked),
 	
-	{state, Frame}.
+	{Frame, State=#state{win=Frame}}.
 
 handle_call(shutdown, _From, State=#state{win=Frame}) ->
     wxFrame:destroy(Frame),
     {stop, normal, ok, State}.
 
 handle_event(#wx{event = #wxClose{}}, State) ->
-	io:format("CALL!!!"),
 	{stop, normal, State};
 handle_event(#wx{id = ?wxID_EXIT, event = #wxCommand{type = command_button_clicked}}, State=#state{win=Frame}) ->
-	io:format("CALL!!!"),
 	wxFrame:destroy(Frame),
 	{stop, normal, State}.
 
@@ -66,9 +64,8 @@ code_change(_, _, State) ->
     {stop, not_yet_implemented, State}.
     
 terminate(_Reason, State=#state{win=Frame}) ->
-	io:format("TERMINATE!!!"),
     wxFrame:destroy(Frame).
 	
-set_info(StaticText) ->
-	%wxStaticText:setLabel(StaticText, ?INFO).
-	ok.
+set_info(StaticText, Info) ->
+	wxStaticText:setLabel(StaticText, Info).
+	
