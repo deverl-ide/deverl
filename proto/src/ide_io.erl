26ea4d4ad9800cf9efc6c0be29e4cd596fdc83d6
mdq3 <mdq3@kent.ac.uk>
2013-07-04 14:52:03 +0100

    modified:   proto/src/ide_io.erl

--- proto/src/ide_io.erl
+++ proto/src/ide_io.erl
@@ -8,28 +8,16 @@ open_file(Parent) ->
 	wxFileDialog:showModal(Dialog),
 
 	Path = wxFileDialog:getPath(Dialog),
-	
 	Filename = wxFileDialog:getFilename(Dialog),
 	
-	File = file:open(Path, [read, write]),
-	
 	{ok, Contents} = file:read_file(Path),
-	
-	io:format(Path ++ "~n"),
-	io:format(Filename ++ "~n"),
-	io:format("~p~n",[binary_to_list(Contents)]),
 	{Filename, binary_to_list(Contents)}.
 	  
 save(Parent, Contents) ->
   Dialog = wxFileDialog:new(Parent, [{style, ?wxFD_SAVE}]),
   wxFileDialog:showModal(Dialog),
   Path = wxFileDialog:getPath(Dialog),
-  
-<<<<<<< HEAD
-  Handle = file:open(Path, [write]),
-  file:write(Handle,Contents).
-=======
   {ok, Fd} = file:open(Path, [read, write, raw]),
   file:write(Fd, Contents),
   file:close(Fd).
->>>>>>> 221d6bac7ef238d961f2d10e909d674637de2bc0
+
