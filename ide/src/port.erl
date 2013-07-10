54929dca9936dee113e1b5d5f5c4c4c4c1022707
Tommo <me@tomrichmond.co.uk>
2013-07-10 13:56:35 +0100

    Port

---
+++ ide/src/port.erl
@@ -0,0 +1,30 @@
+-module(port).
+
+
+%%     /usr/local/lib/erlang/erts-5.10.1/bin/erl
+%% TESTING GIT 
+
+-compile(export_all).
+
+start()->
+    register(port, spawn(?MODULE, read, [])).
+   
+read() ->
+  Port = open_port({spawn,"/usr/local/lib/erlang/erts-5.10.1/bin/erl"},[]),
+  do_read(Port).
+
+do_read(Port) ->
+  receive
+    {Port,{data,Data}} ->
+        io:format("Data: ~p~n",[Data]);
+    {Port,eof} ->
+      read();
+    {go, Go} ->
+        Port ! {self(), {command, Go}};
+    Any ->
+      io:format("MESSAGE, ~p~n",[Any])
+  end,
+  do_read(Port).
+
+call_port(Message) ->
+  port ! Message. 
