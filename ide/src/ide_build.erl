e9781b8d4ce0534ad069773f0bc3a17adef77311
Tommo <me@tomrichmond.co.uk>
2013-09-05 14:37:41 +0100

    Refactoring

--- ide/src/ide_build.erl
+++ ide/src/ide_build.erl
@@ -1,17 +1,21 @@
 -module(ide_build).
 
+-export([
+	compile/0
+]).
+
 
 compile() ->
-  Editor = case doc_manager:get_selected_editor() of
+  case doc_manager:get_selected_editor() of
     {error, _} ->
       ok;
     {ok, {Index, Pid}} ->
       doc_manager:save_file(Index, Pid),
       Path = filename:rootname(editor:get_editor_path(Pid)),
       compile_file(Path)
-  end,
+  end.
   
 
 compile_file(Path) ->
-	port:call_port("c(" ++ Path ++ ")." ++ "io_lib:nl()).
-	
+	io:format("Path: ~p~n", [Path]),
+	port:call_port("c(\"" ++ Path ++ "\")." ++ io_lib:nl()).
