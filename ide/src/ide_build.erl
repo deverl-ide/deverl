72fc3a3400b457371aa207999555bec5d8c29023
mdq3 <mdq3@kent.ac.uk>
2013-09-05 14:19:08 +0100

    new file:   ide/src/ide_build.erl

---
+++ ide/src/ide_build.erl
@@ -0,0 +1,17 @@
+-module(ide_build).
+
+
+compile() ->
+  Editor = case doc_manager:get_selected_editor() of
+    {error, _} ->
+      ok;
+    {ok, {Index, Pid}} ->
+      doc_manager:save_file(Index, Pid),
+      Path = filename:rootname(editor:get_editor_path(Pid)),
+      compile_file(Path)
+  end,
+  
+
+compile_file(Path) ->
+	port:call_port("c(" ++ Path ++ ")." ++ "io_lib:nl()).
+	
