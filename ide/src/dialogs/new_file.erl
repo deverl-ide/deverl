fa5ea2da2b20bc567f43d39c33f70b9bf7b943f2
mdq3 <mdq3@kent.ac.uk>
2013-09-20 13:11:33 +0100

    modified:   dialogs/new_file.erl

--- ide/src/dialogs/new_file.erl
+++ ide/src/dialogs/new_file.erl
@@ -24,69 +24,58 @@ start(Config) ->
 	wx_object:start_link(?MODULE, Config, []).
 
 init(_Args) ->
-	Frame = wxDialog:new(wx:null(), ?wxID_ANY, "New File", [{size,{500, 500}}]),
+	Frame = wxDialog:new(wx:null(), ?wxID_ANY, "New File", [{size,{500, 300}}]),
 	Panel = wxPanel:new(Frame),
   LRSizer = wxBoxSizer:new(?wxHORIZONTAL),
   wxPanel:setSizer(Panel, LRSizer),
   wxSizer:addSpacer(LRSizer, 20),
   
-  
 	MainSizer = wxBoxSizer:new(?wxVERTICAL),
+  wxSizer:addSpacer(MainSizer, 40),
 	wxSizer:add(LRSizer, MainSizer, [{proportion, 1}, {flag, ?wxEXPAND}]),
-
 
 	%%%%%%%%
   
   FlexGridSizer = wxFlexGridSizer:new(2, 2, 10, 10),
 
-  ProjectText   = wxStaticText:new(Panel, ?wxID_ANY, "Project:"),
-  ProjectChoice = wxChoice:new(Panel, ?wxID_ANY),
-  wxSizer:add(FlexGridSizer, ProjectText,   []),
-  wxSizer:add(FlexGridSizer, ProjectChoice, [{proportion, 1}, {flag, ?wxEXPAND}]),
+  wxSizer:add(FlexGridSizer, wxStaticText:new(Panel, ?wxID_ANY, "Project:"),   []),
+  wxSizer:add(FlexGridSizer, wxChoice:new(Panel, ?wxID_ANY), [{proportion, 1}, {flag, ?wxEXPAND}]),
 
-  TypesText  = wxStaticText:new(Panel, ?wxID_ANY, "File Type:"),
-  TypesList  = wxListBox:new(Panel, ?wxID_ANY),
-  wxSizer:add(FlexGridSizer, TypesText, []),
-  wxSizer:add(FlexGridSizer, TypesList, [{proportion, 1}, {flag, ?wxEXPAND}]),
+  wxSizer:add(FlexGridSizer, wxStaticText:new(Panel, ?wxID_ANY, "File Type:"), []),
+  wxSizer:add(FlexGridSizer, wxListBox:new(Panel, ?wxID_ANY), [{proportion, 1}, {flag, ?wxEXPAND}]),
 
-  DescriptionText   = wxStaticText:new(Panel, ?wxID_ANY, "Description:"),
-  DescriptionBox   = wxTextCtrl:new(Panel, ?wxID_ANY),
-  wxSizer:add(FlexGridSizer, DescriptionText,  []),
-  wxSizer:add(FlexGridSizer, DescriptionBox,   [{proportion, 1}, {flag, ?wxEXPAND}]),
+  wxSizer:add(FlexGridSizer, wxStaticText:new(Panel, ?wxID_ANY, "Description:"), []),
+  wxSizer:add(FlexGridSizer, wxTextCtrl:new(Panel, ?wxID_ANY, [{style, ?wxTE_MULTILINE bor ?wxTE_READONLY}]), [{proportion, 1}, {flag, ?wxEXPAND}]),
+  wxFlexGridSizer:addGrowableCol(FlexGridSizer, 1),
+  wxFlexGridSizer:addGrowableCol(FlexGridSizer, 2),
   
-  wxSizer:add(MainSizer, FlexGridSizer, [{proportion, 1}, {flag, ?wxEXPAND}]),
+  wxSizer:add(MainSizer, FlexGridSizer, [{proportion, 0}, {flag, ?wxEXPAND}]),
 
   %%%%%%%%
 
   ButtonSizer = wxBoxSizer:new(?wxHORIZONTAL),
 
-  BackButton   = wxButton:new(Panel, ?BACK,      [{label, "Back"}]),
-  NextButton   = wxButton:new(Panel, ?NEXT,      [{label, "Next"}]),
-  FinishButton = wxButton:new(Panel, ?FINISH,    [{label, "Finish"}]),
-  CancelButton = wxButton:new(Panel, ?wxID_EXIT, [{label, "Cancel"}]),
-
-  wxSizer:add(ButtonSizer, BackButton,   [{border, 2}, {flag, ?wxALL bor ?wxALIGN_RIGHT}]),
-  wxSizer:add(ButtonSizer, NextButton,   [{border, 2}, {flag, ?wxALL bor ?wxALIGN_RIGHT}]),
-  wxSizer:add(ButtonSizer, FinishButton, [{border, 2}, {flag, ?wxALL bor ?wxALIGN_RIGHT}]),
-  wxSizer:add(ButtonSizer, CancelButton, [{border, 2}, {flag, ?wxALL bor ?wxALIGN_RIGHT}]),
+  wxSizer:add(ButtonSizer, wxButton:new(Panel, ?BACK,      [{label, "Back"}]),   [{border, 2}, {flag, ?wxALL}]),
+  wxSizer:add(ButtonSizer, wxButton:new(Panel, ?NEXT,      [{label, "Next"}]),   [{border, 2}, {flag, ?wxALL}]),
+  wxSizer:add(ButtonSizer, wxButton:new(Panel, ?FINISH,    [{label, "Finish"}]), [{border, 2}, {flag, ?wxALL}]),
+  wxSizer:add(ButtonSizer, wxButton:new(Panel, ?wxID_EXIT, [{label, "Cancel"}]), [{border, 2}, {flag, ?wxALL}]),
   
-  wxSizer:add(MainSizer, ButtonSizer, [{proportion, 1}, {flag, ?wxEXPAND}]),
+  wxSizer:add(MainSizer, ButtonSizer, [{proportion, 1}, {flag, ?wxALIGN_RIGHT}]),
 
   %%%%%%%%
 
-  %PageTwoSizer = wxBoxSizer:new(?wxVERTICAL),
-
-  %FileNameSizer      = wxBoxSizer:new(?wxHORIZONTAL),
-  %ChosenProjectSizer = wxBoxSizer:new(?wxHORIZONTAL),
-  %FolderSizer        = wxBoxSizer:new(?wxHORIZONTAL),
-  %PathSizer          = wxBoxSizer:new(?wxHORIZONTAL),
-
-  %%%%%%%%
+  wxSizer:addSpacer(LRSizer, 20),
 
 	wxDialog:showModal(Frame),
 
 	State = #state{win = Frame},
 	{Frame, State}.
+
+
+%% =====================================================================
+%% OTP callbacks
+%%
+%% =====================================================================
 
 handle_cast(_Msg, State) ->
 	io:format("handle_cast/2: NEW FILE DIALOG"),
@@ -112,6 +101,10 @@ code_change(_, _, State) ->
 terminate(_Reason, #state{win=Frame}) ->
   wxDialog:destroy(Frame).
 
+
+dialog2() ->
+  ok.
+  
 
 
 
