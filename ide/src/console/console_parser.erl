0beedd6ff0e51e36258fedc97df131b415f53126
mdq3 <mdq3@kent.ac.uk>
2013-11-25 12:08:43 +0000

    modified:   ide/src/console/console_parser.erl 	modified:   ide/src/console/console_wx.erl 	modified:   ide/src/lib_widgets.erl 	modified:   ide/src/node.erl

--- ide/src/console/console_parser.erl
+++ ide/src/console/console_parser.erl
@@ -38,7 +38,20 @@ parse_input(Message) ->
 %% @doc
 
 parse_response(Response) ->
-  % io:format("RESPONSE FROM PORT~n"),
-  M = Response,
+  io:format("RESPONSE FROM PORT: ~p~n", [Response]),
+  M = check_for_prompt(Response),
 	console_wx:append_to_console(M).
   
+  
+%% =====================================================================
+%% @doc
+
+check_for_prompt(Response) ->
+  case re:run(Response, "(.+)\\d> $", [{capture, [1]}]) of
+    nomatch ->
+      "";
+    {match, [{Start, Length}]} ->
+      R = string:substr(Response, Start, Length),
+      io:format("RESPONSE: ~p~n", [R]),
+      R
+  end.
