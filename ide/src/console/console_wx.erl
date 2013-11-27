da4c9d90928b1f54fdc8205a6b7233553bc38160
mdq3 <mdq3@kent.ac.uk>
2013-11-27 11:03:14 +0000

    modified:   ide/src/console/console_wx.erl

--- ide/src/console/console_wx.erl
+++ ide/src/console/console_wx.erl
@@ -3,7 +3,7 @@
 %% @copyright
 %% @title
 %% @version
-%% @doc This module initalises the console, which is currently 
+%% @doc This module initalises the console, which is currently
 %% implemented as a wxStyledTextCtrl.
 %% @end
 %% =====================================================================
@@ -14,16 +14,16 @@
 
 %% wx_object
 -behaviour(wx_object).
--export([init/1, 
-		 		 terminate/2, 
-				 code_change/3, 
-				 handle_info/2, 
-				 handle_call/3, 
-				 handle_cast/2, 
+-export([init/1,
+		 		 terminate/2,
+				 code_change/3,
+				 handle_info/2,
+				 handle_call/3,
+				 handle_cast/2,
 				 handle_event/2,
 				 handle_sync_event/3]).
 
-%% API     
+%% API
 -export([new/1,
 				 append_command/1,
          append_message/1,
@@ -40,40 +40,40 @@
 -define(MARKER_MSG, 1).
 
 %% Server state
--record(state, {win, 
-								textctrl, 
-								cmd_history, 
-								current_cmd, 
+-record(state, {win,
+								textctrl,
+								cmd_history,
+								current_cmd,
 								wx_env,
                 menu}).
 
 
 %% =====================================================================
 %% Client API
-%% =====================================================================	
+%% =====================================================================
 
 %% =====================================================================
 %% @doc
 
 new(Config) ->
 	wx_object:start_link({local, ?MODULE}, ?MODULE, Config, []).
-	
-	
+
+
 %% =====================================================================
 %% @doc
-	
+
 append_command(Response) ->
   wx_object:cast(?MODULE, {append, Response}).
 
-	
-  
+
+
 %% =====================================================================
 %% @doc
-	
+
 append_message(Msg) ->
   wx_object:cast(?MODULE, {append_msg, Msg}).
-  
-  	
+
+
 %% =====================================================================
 %% @doc Update the shell's theme
 
@@ -82,11 +82,11 @@ set_theme(Fg, Bg, MrkrBg, ErrFg) ->
 
 
 %% =====================================================================
-%% @doc 
+%% @doc
 
 set_font(Font) ->
-  wx_object:cast(?MODULE, {set_font, Font}).	
-  
+  wx_object:cast(?MODULE, {set_font, Font}).
+
 
 %% =====================================================================
 %% Callback functions
@@ -104,56 +104,56 @@ init(Config) ->
 	?stc:setMarginWidth(Console, 2, 0),
 	?stc:setMarginLeft(Console, 2),
 	?stc:setLexer(Console, ?wxSTC_LEX_NULL),
-  
-	?stc:styleSetFont(Console, ?wxSTC_STYLE_DEFAULT, 
+
+	?stc:styleSetFont(Console, ?wxSTC_STYLE_DEFAULT,
 					  wxFont:new(13, ?wxFONTFAMILY_TELETYPE, ?wxNORMAL, ?wxNORMAL,[])),
-	?stc:setCaretWidth(Console, 1), 
+	?stc:setCaretWidth(Console, 1),
 	?stc:cmdKeyClear(Console, ?wxSTC_KEY_UP, 0),
-  
+
   %% Saved styles
   {_Name, Fg, Bg, MrkrBg, ErrFg} = sys_pref_manager:get_preference(console_theme),
   set_theme(Fg, Bg, MrkrBg, ErrFg),
   set_font(sys_pref_manager:get_font(console)),
-  
+
   %% Alternate styles
   ?stc:styleSetSpec(Console, ?STYLE_PROMPT, "bold,underline"), % For the prompt
   ?stc:styleSetBold(Console, ?STYLE_PROMPT, true),
-  
+
   %% Markers
   ?stc:markerDefine(Console, ?MARKER_MSG, ?wxSTC_MARK_BACKGROUND),
   ?stc:markerSetBackground(Console, ?MARKER_MSG, MrkrBg),
-                                                	
+
 	wxSizer:add(MainSizer, Console, [{flag, ?wxEXPAND}, {proportion, 1}]),
-                                          
+
   Menu = create_menu(),
-                                          
+
 	?stc:connect(Console, key_down, [callback]),
   ?stc:connect(Console, right_up),
-  
+
   %% Add initial text
   InitText = "Erlang Evaluator V0.2\n" ++ ?PROMPT,
   ?stc:setText(Console, InitText),
   ?stc:startStyling(Console, 0, 31),
   ?stc:setStyling(Console, length(InitText) - 1, ?STYLE_PROMPT),
 
-	State=#state{win=Panel, 
-				       textctrl=Console, 
+	State=#state{win=Panel,
+				       textctrl=Console,
 				       cmd_history=[],
 				       current_cmd=0,
                menu=Menu},
-  
+
   {Panel, State}.
 
 handle_info(Msg, State) ->
   io:format("Got cast ~p~n",[Msg]),
   {noreply,State}.
-  
+
 handle_cast({set_theme, Fg, Bg, MrkrBg, ErrFg}, State=#state{textctrl=Console}) ->
   ?stc:styleSetBackground(Console, 0, Bg),
   ?stc:styleSetForeground(Console, 0, Fg),
   ?stc:styleSetBackground(Console, ?wxSTC_STYLE_DEFAULT, Bg),
   ?stc:styleSetForeground(Console, ?wxSTC_STYLE_DEFAULT, Fg),
-  ?stc:styleSetBackground(Console, ?STYLE_PROMPT, Bg),  
+  ?stc:styleSetBackground(Console, ?STYLE_PROMPT, Bg),
   ?stc:styleSetForeground(Console, ?STYLE_PROMPT, Fg),
   ?stc:setCaretForeground(Console, Fg),
   ?stc:markerSetBackground(Console, ?MARKER_MSG, MrkrBg),
@@ -176,7 +176,7 @@ handle_cast({append_msg, Msg}, State=#state{textctrl=Console}) ->
   ?stc:newLine(Console),
   ?stc:gotoPos(Console, ?stc:getLength(Console)),
   {noreply, State}.
-  
+
 handle_call(text_ctrl, _From, State) ->
   {reply,{State#state.wx_env,State#state.textctrl}, State};
 handle_call(command_history, _From, State) ->
@@ -190,7 +190,7 @@ handle_call({update_cmd_index, Index}, _From, State) ->
 handle_call(Msg, _From, State) ->
   io:format("Got Call ~p~n",[Msg]),
   {reply,ok,State}.
-    
+
 handle_event(#wx{obj=Console, event=#wxMouse{type=right_up}},
             State=#state{menu=Menu}) ->
   wxWindow:popupMenu(Console, Menu),
@@ -210,7 +210,7 @@ handle_event(#wx{obj=Console, id=Id, event=#wxCommand{type=command_menu_selected
   %     io:format("NOT IMPLEMENTED")
   % end,
 	{noreply, State}.
-    
+
 code_change(_, _, State) ->
 	{stop, not_yet_implemented, State}.
 
@@ -227,7 +227,7 @@ handle_sync_event(#wx{obj=Console, event=#wxKey{type=key_down, keyCode=13}}, Eve
   {Prompt,Input} = split_line_at_prompt(Console),
   ?stc:gotoPos(Console, ?stc:getLength(Console)),
   case length(Input) of
-    0 -> 
+    0 ->
       %% Single enter key pressed with no other input.
       %% Note we have manually insert the prompt because sending a single newline '\n'
       %% to the port results in no response. It is the terminal that redraws the prompt
@@ -236,7 +236,7 @@ handle_sync_event(#wx{obj=Console, event=#wxKey{type=key_down, keyCode=13}}, Eve
       prompt_2_console(Console, Prompt),
       call_parser(Input), %% send anyway, so any error contains the correct position integer
       ok;
-    _ ->      
+    _ ->
       Last = fun(46) -> %% keycode 46 = '.'
                %% Deal with the case where several '.'s are entered, '...'
                prompt_or_not(Console, Input, Prompt, Event);
@@ -277,25 +277,25 @@ handle_sync_event(#wx{obj=Console, event=#wxKey{type=key_down, keyCode=?WXK_DOWN
     _ ->
       cycle_cmd_text(Console, 1)
   end;
-handle_sync_event(#wx{obj=Console, event=#wxKey{type=key_down, keyCode=?WXK_LEFT}}, Event, _State) -> 
+handle_sync_event(#wx{obj=Console, event=#wxKey{type=key_down, keyCode=?WXK_LEFT}}, Event, _State) ->
   check_cursor(Console, fun() -> wxEvent:skip(Event) end, fun() -> ok end, 0);
-handle_sync_event(#wx{obj=Console, event=#wxKey{type=key_down, keyCode=?WXK_RIGHT}}, Event, _State) -> 
+handle_sync_event(#wx{obj=Console, event=#wxKey{type=key_down, keyCode=?WXK_RIGHT}}, Event, _State) ->
   check_cursor(Console, fun() -> wxEvent:skip(Event) end, fun() -> ok end, -1);
-handle_sync_event(#wx{obj=Console, event=#wxKey{type=key_down, keyCode=8}}, Event, _State) -> 
-  check_cursor(Console, fun() -> wxEvent:skip(Event) end, fun() -> ok end, 0);	
-handle_sync_event(#wx{obj=Console, event=#wxKey{type=key_down}}, Event, _State) -> 
+handle_sync_event(#wx{obj=Console, event=#wxKey{type=key_down, keyCode=8}}, Event, _State) ->
+  check_cursor(Console, fun() -> wxEvent:skip(Event) end, fun() -> ok end, 0);
+handle_sync_event(#wx{obj=Console, event=#wxKey{type=key_down}}, Event, _State) ->
   SuccessFun = fun() -> wxEvent:skip(Event) end,
   FailFun    = fun() -> ?stc:gotoPos(Console, ?stc:getLength(Console)), wxEvent:skip(Event) end,
   check_cursor(Console, SuccessFun, FailFun, -1).
 
-	
+
 %% =====================================================================
 %% Internal functions
 %% =====================================================================
-	
+
 %% =====================================================================
 %% @doc Write a newline plus the repeated prompt to the console.
-	
+
 prompt_2_console(Console, Prompt) ->
   io:format("PROMPT~n"),
   ?stc:newLine(Console),
@@ -304,30 +304,30 @@ prompt_2_console(Console, Prompt) ->
   ?stc:startStyling(Console, Start, 31),
   ?stc:setStyling(Console, length(Prompt) - 1, ?STYLE_PROMPT),
   ok.
-	
-	
+
+
 %% =====================================================================
 %% @doc Determine whether we need to manually prompt_2_console().
 %% @private
 
 prompt_or_not(Console, Input, Prompt, EvObj) when erlang:length(Input) > 1 ->
-  io:format("INPUT: ~p~n", [Input]),
   Penult = lists:nth(length(Input)-1, Input),
-	if 
-		Penult =:= 46 ->
+	if
+		Penult =:= 46 -> 
 			prompt_2_console(Console, Prompt),
 			wxEvent:stopPropagation(EvObj);
-		true -> 
+		true ->
       wxEvent:skip(EvObj)
+      %prompt_2_console(Console, Prompt)
 	end;
   
 prompt_or_not(_,_,_,EvObj) ->
 	wxEvent:skip(EvObj).
-	
-	
+
+
 %% =====================================================================
 %% @doc
-	
+
 call_parser(Message) ->
   % io:format("Message~p~n", [Message]),
 	console_parser:parse_input(Message).
@@ -337,32 +337,32 @@ call_parser(Message) ->
 %% @doc Separate the prompt and command on the most recent line.
 
 split_line_at_prompt(Console) ->
-  lists:split(length(?PROMPT), %%get_cur_prompt_length(Console), 
-    ?stc:getLine(Console, ?stc:getLineCount(Console) - 1)).	
-	
+  lists:split(length(?PROMPT), %%get_cur_prompt_length(Console),
+    ?stc:getLine(Console, ?stc:getLineCount(Console) - 1)).
+
 
 %% =====================================================================
 %% @doc Get the length of the current prompt.
-	
+
 get_cur_prompt_length(Console) ->
   get_prompt_length(?stc:getLine(Console, ?stc:getLineCount(Console) - 1)).
-	
-  
+
+
 %% =====================================================================
 %% @doc Get the length of the prompt.
-	
+
 get_prompt_length(Line) ->
 	get_prompt_length(Line, 1).
-	
+
 get_prompt_length([], _) -> 0;
 get_prompt_length([Char|String], Count) ->
 	case Char of
 		62 -> % the prompt char '>'
-			Count + 1; %% 
-		_ -> 
+			Count + 1; %%
+		_ ->
 			get_prompt_length(String, Count + 1)
 	end.
-	
+
 
 %% =====================================================================
 %% @doc
@@ -370,8 +370,8 @@ get_prompt_length([Char|String], Count) ->
 
 get_line_start_pos(Console, Pos) ->
   ?stc:positionFromLine(Console, ?stc:lineFromPosition(Console, Pos)).
-	
-  
+
+
 %% =====================================================================
 %% @doc Append new command to end of command history iff last element not same as new element.
 
@@ -390,8 +390,8 @@ add_cmd(Command) ->
 					update_cmd_index(length(CommandList)+1)
 			end
 	end.
-	
-	
+
+
 %% =====================================================================
 %% @doc Cycle through command history by one entry.
 %% param Direction: -1 for prev cmd, +1 for next cmd.
@@ -400,8 +400,8 @@ cycle_cmd_text(Console, Direction) ->
 	update_cmd_index(cmd_index() + Direction),
 	Cmd = get_command(cmd_index()),
   replace_cmd_text(Console, Cmd).
-	
-	
+
+
 %% =====================================================================
 %% @doc Replace the current command with the updated command Cmd.
 %% The replacement will always take place on the most recent line.
@@ -415,34 +415,34 @@ replace_cmd_text(Console, Cmd) ->
 
 %% =====================================================================
 %% @doc
-	
+
 cmd_index() ->
 	{CommandIndex} = wx_object:call(?MODULE, command_index),
 	CommandIndex.
-	
-	
+
+
 %% =====================================================================
 %% @doc
 
 cmd_history() ->
 	{CommandList} = wx_object:call(?MODULE, command_history),
 	CommandList.
-	
-	
+
+
 %% =====================================================================
 %% @doc
 
 update_cmd_index(NewIndex) ->
 	wx_object:call(?MODULE, {update_cmd_index, NewIndex}).
-	
-	
+
+
 %% =====================================================================
 %% @doc
 
 update_cmd_history(CommandList, Command) ->
 	wx_object:call(?MODULE, {update_cmd_history, CommandList ++ [Command]}).
-	
-	
+
+
 %% =====================================================================
 %% @doc Retrieve command from history based on indexed position.
 
@@ -454,9 +454,9 @@ get_command(Index, [H|T], Count) ->
 			H;
 		false ->
 			get_command(Index, T, Count + 1)
-	end.	
-	
-	
+	end.
+
+
 %% =====================================================================
 %% @doc Replace element at given index in a list. (Not yet used)
 
@@ -469,22 +469,22 @@ replace(Index, Elem, [H|T], Count, Acc) ->
 		false ->
 			replace(Index, Elem, T, Count + 1, Acc ++ [H])
 	end.
-	
-	
+
+
 %% =====================================================================
 %% @doc Check cursor is in valid position, and execute appropriate function.
-	
+
 check_cursor(Console, SuccessFun, FailFun, PromptOffset) ->
-  Limit = get_line_start_pos(Console, ?stc:getLength(Console)) + 
+  Limit = get_line_start_pos(Console, ?stc:getLength(Console)) +
     get_cur_prompt_length(Console),
   case (?stc:getCurrentPos(Console) > Limit + PromptOffset)  of
-    true -> 
+    true ->
       SuccessFun();
     false ->
       FailFun()
   end,
   ok.
-  
+
 
 %% =====================================================================
 %% @doc Build the popup "right click" menu.
