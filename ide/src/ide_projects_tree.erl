33fc63f5793e3d456768f467dbaeda057a010c87
Tom Richmond <me@tomrichmond.co.uk>
2013-12-10 14:25:48 +0000

    MSW testing wxTreeCtrl handlers.

--- ide/src/ide_projects_tree.erl
+++ ide/src/ide_projects_tree.erl
@@ -259,6 +259,7 @@ handle_event(#wx{obj=Tree, event=#wxTree{type=command_tree_item_expanding, item=
       io:format("INSERTING~n"),
       {_, FilePath} = wxTreeCtrl:getItemData(Tree, Item),
       insert(Tree, Item, FilePath),
+      io:format("INSERTED~n"),
       wxTreeCtrl:toggle(Tree, Item),
       % wxTreeCtrl:expand(Tree, Item),
  %      receive after 2000 -> ok end,
