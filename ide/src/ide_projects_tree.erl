d5c635a608c49b024a097378bc880fa06e80d345
Tom Richmond <me@tomrichmond.co.uk>
2013-12-10 11:32:47 +0000

    MSW testing wxTreeCtrl handlers.

--- ide/src/ide_projects_tree.erl
+++ ide/src/ide_projects_tree.erl
@@ -179,7 +179,7 @@ init(Config) ->
 
 	wxSizer:add(MainSz, Tree, [{proportion, 1}, {flag, ?wxEXPAND}]),
 
-  wxTreeCtrl:connect(Tree, command_tree_item_activated, []),
+  % wxTreeCtrl:connect(Tree, command_tree_item_activated, []),
   wxTreeCtrl:connect(Tree, command_tree_sel_changed, []),
   % wxTreeCtrl:connect(Tree, command_tree_sel_changing, [callback]), %% To veto a selection
   wxTreeCtrl:connect(Tree, command_tree_item_expanding, []),
@@ -252,7 +252,7 @@ handle_event(#wx{obj=Tree, event=#wxTree{type=command_tree_item_expanding, item=
     true ->
       {_, FilePath} = wxTreeCtrl:getItemData(Tree, Item),
       insert(Tree, Item, FilePath),
-      receive after 2000 -> ok end,
+      % receive after 2000 -> ok end,
       % alternate_background_all(Tree),
       ok;
     false -> ok
