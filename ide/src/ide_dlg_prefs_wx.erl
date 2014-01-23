78381839bd230335ac253169136750bad261492a
Tom Richmond <me@tomrichmond.co.uk>
2014-01-23 12:22:06 +0000

    Completed the XRC structure for the Preferences dialog. When complete all behaviour will be now contained within ide_dlg_prefs_wx, rather than being seperated into individual modules. The orig modules are in misc/previour_versions/preferences.

--- ide/src/ide_dlg_prefs_wx.erl
+++ ide/src/ide_dlg_prefs_wx.erl
@@ -25,7 +25,9 @@
 -export([start/1]).
 
 %% Server state
--record(state, {frame, pref_panel, pref}).        
+-record(state, {frame,
+                cur_pref
+                }).        
 
 -define(PREF_GENERAL, 1).
 -define(PREF_EDITOR, 2).
@@ -47,79 +49,54 @@ start(Config) ->
 %% =====================================================================
 %% Callback functions
 %% =====================================================================
- 
-%% =====================================================================
-%% @doc Initialise the preference pane.
   
 init(Config) ->
   Parent = proplists:get_value(parent, Config),
-	Frame = wxFrame:new(Parent, ?wxID_ANY, "Preferences", []),
+  Xrc = wxXmlResource:get(),
+  Frame = wxFrame:new(),
+  ide_lib_dlg_wx:win_var(Frame),
+  wxXmlResource:loadFrame(Xrc, Frame, Parent, "prefs"),
   
-  ToolBar = wxFrame:createToolBar(Frame, [{style, ?wxTB_TEXT}]),
-  wxToolBar:setToolBitmapSize(ToolBar, {48,48}),
-  %% Id, text, bitmap path, args, add seperator
-  Tools = [{?PREF_GENERAL,  "General", "prefs/general.png",    [],  false},
-           {?PREF_EDITOR, "Editor", "prefs/editor.png",   [],  false},
-           {?PREF_CONSOLE, "Console", "prefs/console.png",   [],  false},
-           {?PREF_DEBUG, "Advanced", "prefs/applications-system.png",   [],  false}],
-
-  AddTool = fun({Id, Tooltip, Filename, Args, true}) ->
-            wxToolBar:addRadioTool(ToolBar, Id, Tooltip, wxBitmap:new(wxImage:new(ide_lib_widgets:rc_dir(Filename))), Args),
-            wxToolBar:addSeparator(ToolBar);
-           ({Id, Tooltip, Filename, Args, _}) ->
-            wxToolBar:addRadioTool(ToolBar, Id, Tooltip, wxBitmap:new(wxImage:new(ide_lib_widgets:rc_dir(Filename))), Args)
-            end,       
-
-  [AddTool(Tool) || Tool <- Tools],
-
-  wxToolBar:realize(ToolBar),
-  wxFrame:connect(Frame, command_menu_selected, [{userData, Tools}]),
-  
-  Panel = wxPanel:new(Frame),
-  PanelSz = wxBoxSizer:new(?wxVERTICAL),
-  wxPanel:setSizer(Panel, PanelSz),
-    
-  State = #state{frame=Frame, pref_panel={Panel, PanelSz}},
-  
-  %% Load the first preference pane
-  PrefPane = load_pref("general", State),
-  
-  wxSizer:add(PanelSz, PrefPane, [{proportion,1}, {flag, ?wxEXPAND}]),
-	wxSizer:setSizeHints(PanelSz, Frame),
-  wxSizer:layout(PanelSz),
+  Connect = fun(UD) ->
+    Id = wxXmlResource:getXRCID("tool_" ++ atom_to_list(UD)),
+    wxFrame:connect(Frame,command_menu_selected,[{id,Id}, {userData,UD}])
+  end,
+  [Connect(Str) || Str <- [general, console, compiler, dialyzer]],
+   
+  %% The initial pref
+  Pref = wxXmlResource:xrcctrl(Frame, "console", wxPanel),
+  F0 = ide_sys_pref_gen:get_font(console_font),
+  FStr0 = wxXmlResource:xrcctrl(Frame, "console_font_st", wxStaticText),
+  wxStaticText:setLabel(FStr0, get_font_string(F0)),
+  Themes = wxXmlResource:xrcctrl(Frame, "console_themes_p", wxPanel),
+  add_themes(Themes),
   
   wxFrame:centre(Frame),
-  wxFrame:show(Frame),
-  
-  {Frame, State#state{pref=PrefPane}}.
+  wxFrame:fit(Frame),
+  wxFrame:show(Frame),  
+  {Frame, #state{frame=Frame, cur_pref=Pref}}.
 
 handle_info(Msg, State) ->
-  io:format("Got Info (prefs) ~p~n",[Msg]),
   {noreply,State}.
     
 handle_call(_Msg, _From, State) ->
-  {reply,{error, nyi}, State}.
+  {reply, ok, State}.
     
 handle_cast(Msg, State) ->
   io:format("Got cast ~p~n",[Msg]),
   {noreply,State}.
-    
-%% Catch menu clicks
-handle_event(#wx{id=Id, event=#wxCommand{type=command_menu_selected}, userData=Tb}, 
-             State=#state{frame=Frame, pref_panel={Panel,Sz}, pref=Pref}) ->
-  {_,Str,_,_,_} = proplists:lookup(Id,Tb),
-  wxSizer:detach(Sz, Pref),
-  wx_object:call(Pref, shutdown),
-  wxPanel:hide(Panel), %% Hide whilst loading, and show when complete to stop flicker
-  NewPref = load_pref(Str, State),
-  wxSizer:add(Sz, NewPref, [{proportion,1}, {flag, ?wxEXPAND}]),
-  wxPanel:show(Panel),
-  wxWindow:setClientSize(Frame, wxWindow:getSize(NewPref)),
-  wxWindow:setMinSize(Frame, wxWindow:getSize(NewPref)),
-  {noreply, State#state{pref=NewPref}};
+
+%% Swap pref panels
+handle_event(#wx{event=#wxCommand{type=command_menu_selected}, userData=UD}, 
+             State=#state{frame=Frame, cur_pref=Pref0}) ->
+  Pref1 = wxXmlResource:xrcctrl(Frame, atom_to_list(UD), wxPanel),
+  wxPanel:hide(Pref0),
+  wxPanel:show(Pref1),
+  wxFrame:fit(Frame),
+  {noreply, State#state{cur_pref=Pref1}};
     
 %% Event catchall for testing
-handle_event(Ev = #wx{}, State) ->
+handle_event(Ev=#wx{}, State) ->
   io:format("Prefs event catchall: ~p\n", [Ev]),
   {noreply, State}.
     
@@ -135,9 +112,50 @@ terminate(_Reason, #state{frame=Frame}) ->
 %% =====================================================================
 
 %% =====================================================================
-%% @doc Load a preference pane.
+%% @doc
 
-load_pref(Pref, #state{pref_panel={Panel,_Sz}}) ->
-  ModStr = "ide_pref_" ++ string:to_lower(Pref) ++ "_wx",
-  Mod = list_to_atom(ModStr),
-  Mod:start([{parent, Panel}]).
+-spec get_font_string(wxFont:wxFont()) -> io_lib:chars().
+
+get_font_string(Font) ->
+  io_lib:format("~s ~p pt.", [wxFont:getFaceName(Font), wxFont:getPointSize(Font)]).
+  
+
+add_themes(Parent) ->
+  %% Console themes {Name, FgColour, BgColour, MarkerBg, ErrorFg}
+  Themes = [{"Light", ?wxBLACK, ?wxWHITE, {230,230,230}, ?wxRED},
+            {"Dark", ?wxWHITE, ?wxBLACK, {30,30,30}, {146, 91, 123}},
+            {"Matrix", {0,204,0}, ?wxBLACK, {30,30,30}, {146, 91, 123}}],
+            
+  SavedTheme = ide_sys_pref_gen:get_preference(console_theme),  
+  % Sz = wxGridSizer:new(3, [{vgap, 5}, {hgap, 5}]),
+  Sz = wxBoxSizer:new(?wxHORIZONTAL),
+  Add = fun({Name, Fg, Bg, Mrkr, Err}=Theme) ->
+    Sz1 = wxBoxSizer:new(?wxHORIZONTAL),
+    Sample = wxPanel:new(Parent, [{style, ?wxBORDER_SIMPLE}, {size, {65,40}}]),
+    wxPanel:setMinSize(Sample, {65, 40}),
+    T = wxStaticText:new(Sample, ?wxID_ANY, "1> 1+1.\n3\n2>"),
+    wxWindow:setFont(T, wxFont:new(8, ?wxFONTFAMILY_TELETYPE, ?wxNORMAL, ?wxNORMAL)),
+    wxWindow:setForegroundColour(T, Fg),
+    wxWindow:setBackgroundColour(Sample, Bg),
+    Radio = wxRadioButton:new(Parent, ?wxID_ANY, Name, []),
+    wxSizer:add(Sz1, Sample, [{flag, ?wxALIGN_CENTRE}]),
+    wxSizer:add(Sz1, Radio, [{flag, ?wxALIGN_CENTRE}]),
+    wxSizer:add(Sz, Sz1, [{flag, ?wxALIGN_CENTRE}]),
+    wxSizer:layout(Sz),
+    wxPanel:layout(Sample),
+    case SavedTheme of
+      Theme ->
+        wxRadioButton:setValue(Radio, true);
+      _ ->
+        ok
+    end,
+    % Evt Handler
+    Update = fun(_E,_O) ->
+      ide_console_wx:set_theme(Fg, Bg, Mrkr, Err),
+      ide_sys_pref_gen:set_preference(console_theme, Theme)
+    end,
+    wxRadioButton:connect(Radio, command_radiobutton_selected, [{callback,Update}])
+  end, 
+  [ Add(Theme) || Theme <- Themes ],
+  wxPanel:setSizer(Parent, Sz),
+  wxPanel:fit(Parent).
