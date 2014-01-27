5871710b55f19273906f4f1e69a4c72fe1dbe40a
mdq3 <mdq3@kent.ac.uk>
2014-01-27 12:08:40 +0000

    Test implementation: set indicator icons based on test results. 	modified:   ide/src/ide.erl 	modified:   ide/src/ide_testpane.erl

--- ide/src/ide_testpane.erl
+++ ide/src/ide_testpane.erl
@@ -5,10 +5,17 @@
 
 -export([
   new/1,
-  add_module_tests/1
+  add_module_tests/1,
+  show_test_results/1
   ]).
 
+%% The test list.
 -define(ID_LIST, 1023).
+
+%% The test indicator icons
+-define(TEST_UNTESTED, 0).
+-define(TEST_SUCCESS,  1).
+-define(TEST_FAIL,     2).
 
 
 %% =====================================================================
@@ -20,27 +27,27 @@ new(Config) ->
   List = wxListCtrl:new(Panel, [{winid, ?ID_LIST}, {style, ?wxLC_REPORT bor ?wxLC_NO_HEADER bor ?wxLC_SINGLE_SEL bor ?wxBORDER_NONE}]),
   wxListCtrl:insertColumn(List, 0, "Heading", []),
   wxListCtrl:setColumnWidth(List, 0, ?wxLIST_AUTOSIZE),
-  
+
   Resize = fun(_EvtRec, WxEvt) ->
     {W,_H} = wxSizeEvent:getSize(WxEvt),
     wxListCtrl:setColumnWidth(List, 0, W),
     wxEvent:skip(WxEvt)
   end,
   wxListCtrl:connect(List, size, [{callback, Resize}]),
-  
+
   %% Images
 	ImgList = wxImageList:new(20,20),
 	wxImageList:add(ImgList, wxBitmap:new(wxImage:new(ide_lib_widgets:rc_dir("test_empty.png")))),
 	wxImageList:add(ImgList, wxBitmap:new(wxImage:new(ide_lib_widgets:rc_dir("test_success.png")))),
 	wxImageList:add(ImgList, wxBitmap:new(wxImage:new(ide_lib_widgets:rc_dir("test_fail.png")))),
   wxListCtrl:assignImageList(List, ImgList, ?wxIMAGE_LIST_SMALL),
-  
+
   Sz = wxBoxSizer:new(?wxVERTICAL),
   wxSizer:add(Sz, List, [{flag, ?wxEXPAND}, {proportion, 1}]),
   wxPanel:setSizer(Panel, Sz),
   Panel.
-  
-  
+
+
 %% =====================================================================
 %% @doc
 
@@ -49,16 +56,46 @@ add_module_tests(Module) ->
   code:delete(Module),
   List = wx:typeCast(wxWindow:findWindowById(?ID_LIST), wxListCtrl),
   wxListCtrl:deleteAllItems(List),
-  try 
+  try
     Tests = eunit_data:get_module_tests(Module),
     insert(List, Tests)
   catch
-    throw:_E -> 
+    throw:_E ->
       % List placeholder
       ok
   end.
-  
-  
+
+
+%% =====================================================================
+%% @doc Set the test indicator icon to signify test pass or failure.
+
+-spec show_test_results(Results) -> ok when
+  Results :: list({string(), boolean()}).
+
+show_test_results(Results) ->
+  List = wx:typeCast(wxWindow:findWindowById(?ID_LIST), wxListCtrl),
+  show_test_results(Results, List).
+
+-spec show_test_results(Results, wxListCtrl:wxListCtrl()) -> ok when
+  Results :: list({string(), boolean()}).
+
+show_test_results([], _List) ->
+  ok;
+show_test_results([{FunctionName, Result}|Results], List) ->
+  Item = wxListCtrl:findItem(List, -1, FunctionName),
+  case Result of
+    true ->
+      wxListCtrl:setItemImage(List, Item, ?TEST_SUCCESS);
+    false ->
+      wxListCtrl:setItemImage(List, Item, ?TEST_FAIL)
+  end,
+  show_test_results(Results, List).
+
+
+%% =====================================================================
+%% Internal functions
+%% =====================================================================
+
 %% =====================================================================
 %% @doc
 
@@ -67,9 +104,9 @@ insert(ListCtrl, L) ->
     Str = atom_to_list(F),
 		wxListCtrl:insertItem(ListCtrl, Acc, ""),
 		wxListCtrl:setItem(ListCtrl, Acc, 0, Str),
-    wxListCtrl:setItemImage(ListCtrl, Acc, 1),
+    wxListCtrl:setItemImage(ListCtrl, Acc, ?TEST_UNTESTED),
     wxListCtrl:refreshItem(ListCtrl, Acc),
     Acc + 1
   end,
   lists:foldl(Fn, 0, L).
-  
+
