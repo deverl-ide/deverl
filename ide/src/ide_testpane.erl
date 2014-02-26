31dc1f1639eaf136af048679aef6b147857a1fcd
Tom Richmond <me@tomrichmond.co.uk>
2014-02-26 13:52:36 +0000

    Minor fixes.

--- ide/src/ide_testpane.erl
+++ ide/src/ide_testpane.erl
@@ -79,9 +79,14 @@ add_module_tests(Module) ->
 %% =====================================================================
 %% @doc
 
-clear() ->
+clear() ->  
   List = wx:typeCast(wxWindow:findWindowById(?ID_LIST), wxListCtrl),
-  wxListCtrl:deleteAllItems(List).
+  case wx:is_null(List) of
+    true -> ok; %% notebook_empty cast sent after test pane is destroyed
+    false ->  wxListCtrl:deleteAllItems(List)
+  end,
+  io:format("LISTCTRL: ~p~n", [List]),
+  io:format("IS NULL: ~p~n", [wx:is_null(List)]).
 
 
 %% =====================================================================
